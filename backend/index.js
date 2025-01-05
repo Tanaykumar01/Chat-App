985671d5b2012aa3a46a7fd680316333e6cbadbc
Tanaykumar01 <tanayjhunjhunwala.official@gmail.com>
2025-01-06 02:02:25 +0530

    implement socket.io for real-time communication and add SocketContext for managing socket connections

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { server } from "./socket/socket.js";
 import { app } from "./server.js";
 dotenv.config({
     path: './.env'
 })
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 6000, () => {
+    server.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,17 @@
-// server.js
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app } from "./socket/socket.js";
 
-const app = express();
-
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}));
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+// }));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({
     extended: true,
-    limit: "50mb"
+    limit: "50mb",
 }));
 app.use(express.static("public"));
 app.use(cookieParser());
